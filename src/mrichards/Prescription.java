df8a95f4575639b8b672b231658930ffb9b9e44a
Matthew Richards <matthew.richards@stfc.ac.uk>
2018-11-02 10:27:41 +0000

    Add getters and setters, good templates to build around

--- src/mrichards/Prescription.java
+++ src/mrichards/Prescription.java
@@ -1,5 +1,49 @@
 package mrichards;
 
 public class Prescription {
+	private String prescriptionItems;	// Type may be changed
+	private int numberOfPharmaceuticals;
+	private int numberOfContainers;
+	
+	public void addPrescriptionItem(String pharmaceuticalName, int prescribedDailyDose, int duration, boolean counterAvailability, String comments) {
+		
+	}
+	
+	public void removePrescriptionItem(String pharmaceuticalName) {
+		// Remove the PrescriptionItem from the prescription
+	}
+	
+	public void clearPrescription() {
+		// Remove all items from the prescription
+	}
 
+	/*
+	 * Getters and setters
+	 * Is each getter and setter needed? Consider this at the end
+	 * Any set clauses to be set private?
+	 */
+	
+	public String getPrescriptionItems() {
+		return prescriptionItems;
+	}
+
+	public void setPrescriptionItems(String prescriptionItems) {
+		this.prescriptionItems = prescriptionItems;
+	}
+
+	public int getNumberOfPharmaceuticals() {
+		return numberOfPharmaceuticals;
+	}
+
+	public void setNumberOfPharmaceuticals(int numberOfPharmaceuticals) {
+		this.numberOfPharmaceuticals = numberOfPharmaceuticals;
+	}
+
+	public int getNumberOfContainers() {
+		return numberOfContainers;
+	}
+
+	public void setNumberOfContainers(int numberOfContainers) {
+		this.numberOfContainers = numberOfContainers;
+	}
 }
--- src/mrichards/PrescriptionItem.java
+++ src/mrichards/PrescriptionItem.java
@@ -1,5 +1,80 @@
 package mrichards;
 
 public class PrescriptionItem {
+	private String pharmaceuticalName;
+	private int prescribedDailyDose;
+	private int duration;
+	private int containerSize;
+	private boolean counterAvailability;
+	private String comments;
+	
+	
+	public PrescriptionItem(String pharmaceuticalName, int prescribedDailyDose, int duration, int containerSize, boolean counterAvailability, String comments) {
+		
+	}
+
+
+	/*
+	 * Getters and setters
+	 */
+	
+	public String getPharmaceuticalName() {
+		return pharmaceuticalName;
+	}
+
+
+	public void setPharmaceuticalName(String pharmaceuticalName) {
+		this.pharmaceuticalName = pharmaceuticalName;
+	}
+
+
+	public int getPrescribedDailyDose() {
+		return prescribedDailyDose;
+	}
+
+
+	public void setDailyDosage(int prescribedDailyDose) {
+		this.prescribedDailyDose = prescribedDailyDose;
+	}
+
+
+	public int getDuration() {
+		return duration;
+	}
+
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+
+	public int getContainerSize() {
+		return containerSize;
+	}
+
+
+	public void setContainerSize(int containerSize) {
+		this.containerSize = containerSize;
+	}
+
+
+	public boolean isCounterAvailability() {
+		return counterAvailability;
+	}
+
+
+	public void setCounterAvailability(boolean counterAvailability) {
+		this.counterAvailability = counterAvailability;
+	}
+
+
+	public String getComments() {
+		return comments;
+	}
+
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
 
 }
