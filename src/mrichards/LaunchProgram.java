96cee3f794be8867c87a49f43fd5f227d6bd8860
Matthew Richards <matthew.richards@stfc.ac.uk>
2018-11-23 13:50:37 +0000

    Add missing semi colon

--- src/mrichards/LaunchProgram.java
+++ src/mrichards/LaunchProgram.java
@@ -4,6 +4,6 @@ public class LaunchProgram {
 
 	public static void main(String[] args) {
 		PharmaceuticalForm pharmaceuticalForm = new PharmaceuticalForm();
-		pharmaceuticalForm.createForm()
+		pharmaceuticalForm.createForm();
 	}
 }
